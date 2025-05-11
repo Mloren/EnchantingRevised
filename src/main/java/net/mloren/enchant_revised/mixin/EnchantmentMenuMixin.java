0a1e860f5786355e8a9a33847dd2d53f18733c1c
Richard Stern <richard@leagueofgeeks.com>
2025-05-11 16:07:06 +1000

    Removed XP cost from vanilla enchanting table

---
+++ src/main/java/net/mloren/enchant_revised/mixin/EnchantmentMenuMixin.java
@@ -0,0 +1,29 @@
+package net.mloren.enchant_revised.mixin;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.EnchantmentMenu;
+import net.mloren.enchant_revised.config.Config;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = EnchantmentMenu.class, priority = 1800)
+public class EnchantmentMenuMixin
+{
+    //Increase the players levels so the enchanting screen buttons can be clicked
+    @Inject(method = "clickMenuButton", at = @At("HEAD"))
+    public void enchant_revised$clickMenuButton1(Player player, int id, CallbackInfoReturnable<Boolean> cir)
+    {
+        if(Config.SERVER.disableXP.get())
+            player.experienceLevel = (player.experienceLevel << 16) + 9999;
+    }
+
+    //Restore the player's level to what it was before they interacted with the enchanting screen
+    @Inject(method = "clickMenuButton", at = @At("RETURN"))
+    public void enchant_revised$clickMenuButton2(Player player, int id, CallbackInfoReturnable<Boolean> cir)
+    {
+        if(Config.SERVER.disableXP.get())
+            player.experienceLevel = player.experienceLevel >> 16;
+    }
+}
---
+++ src/main/java/net/mloren/enchant_revised/mixin/EnchantmentScreenMixin.java
@@ -0,0 +1,89 @@
+package net.mloren.enchant_revised.mixin;
+
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.EnchantmentScreen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.mloren.enchant_revised.config.Config;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(value = EnchantmentScreen.class, priority = 1800)
+public class EnchantmentScreenMixin extends Screen
+{
+    protected EnchantmentScreenMixin(Component title)
+    {
+        super(title);
+    }
+
+    //Set the players level high for the duration of the enchanting screen so you can always afford the enchantment
+    @Inject(method = "render", at = @At("HEAD"))
+    public void enchant_revised$render1(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo callback)
+    {
+        if(Config.SERVER.disableXP.get())
+            this.minecraft.player.experienceLevel = (this.minecraft.player.experienceLevel << 16) + 9999;
+    }
+
+    //Restore player's level to what it was before entering the enchantment screen
+    @Inject(method = "render", at = @At("RETURN"))
+    public void enchant_revised$render2(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo callback)
+    {
+        if(Config.SERVER.disableXP.get())
+            this.minecraft.player.experienceLevel = this.minecraft.player.experienceLevel >> 16;
+    }
+
+    //Remove required level text from tooltip
+    @WrapWithCondition(method = "render", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z", ordinal = 6, remap = false))
+    private boolean enchant_revised$render3(List instance, Object e)
+    {
+        if(Config.SERVER.disableXP.get())
+            return false;
+        return true;
+    }
+
+    //Hide UI that displays levels required
+    @WrapWithCondition(method = "renderBg", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lnet/minecraft/resources/ResourceLocation;IIII)V", ordinal = 2, remap = false))
+    private boolean enchant_revised$renderBg_blitSprite1(GuiGraphics instance, ResourceLocation sprite, int x, int y, int width, int height)
+    {
+        if(Config.SERVER.disableXP.get())
+            return false;
+        return true;
+    }
+
+    @WrapWithCondition(method = "renderBg", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lnet/minecraft/resources/ResourceLocation;IIII)V", ordinal = 5, remap = false))
+    private boolean enchant_revised$renderBg_blitSprite2(GuiGraphics instance, ResourceLocation sprite, int x, int y, int width, int height)
+    {
+        if(Config.SERVER.disableXP.get())
+            return false;
+        return true;
+    }
+
+    @WrapWithCondition(method = "renderBg", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;drawString(Lnet/minecraft/client/gui/Font;Ljava/lang/String;III)I", remap = false))
+    private boolean enchant_revised$renderBg_drawString(GuiGraphics instance, Font font, String text, int x, int y, int color)
+    {
+        if(Config.SERVER.disableXP.get())
+            return false;
+        return true;
+    }
+
+    @ModifyVariable(method = "renderBg", at = @At(
+            value  = "INVOKE",
+            target = "Lnet/minecraft/client/gui/screens/inventory/EnchantmentNames;getRandomName(Lnet/minecraft/client/gui/Font;I)Lnet/minecraft/network/chat/FormattedText;"),
+            ordinal = 7)
+    public int enchant_revised$renderBg_moveText(int value, @Local(ordinal = 6) int i1)
+    {
+        if(Config.SERVER.disableXP.get())
+            return i1 + 8;
+        else
+            return value;
+    }
+}
